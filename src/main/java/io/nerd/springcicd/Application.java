659b3d8a8d914b7b87c9cfe2ed2025e991ede27a
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-04-03 06:13:42 +0200

    Initial commit

---
+++ src/main/java/io/nerd/springcicd/Application.java
@@ -0,0 +1,22 @@
+package io.nerd.springcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @GetMapping("/")
+    public String sayHiGithubActions() {
+        return "Hi Github Actions";
+    }
+}
---
+++ src/test/java/io/nerd/springcicd/SpringPipelineActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nerd.springcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPipelineActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
